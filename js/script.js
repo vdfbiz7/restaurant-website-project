0fe88edcf892afa0aa4ce5e8728da7915279a32c
Victor Dorado <vdorado@hedyla.com>
2022-09-29 21:57:02 +0200

    Book finished, no responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-function myFunction() {
-	var x = document.getElementById("centered_nav");
-	if (x.className === "nav-links") {
-		x.className += " responsive";
-	} else {
-		x.className = "nav-links";
-	}
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = currentDateTime.getMonth() + 1;
+var date = currentDateTime.getDate() + 1;
+
+if (date < 10) {
+	date = "0" + date;
 }
+if (month < 10) {
+	month = "0" + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var checkinElem = document.querySelector("#checkin-date");
+
+checkinElem.setAttribute("min", dateTomorrow);
+
+checkinElem.onchange = function () {
+	checkoutElem.setAttribute("min", this.value);
+};
